f6e769092c2c8df386f40cd6763a5a76ff23e231
Filipe Paiva <filipe.p@soltroiavillas.com>
2018-05-28 07:31:39 +0100

    added address and rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_061908) do
+ActiveRecord::Schema.define(version: 2018_05_28_062813) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
+    t.string "description"
   end
 
 end
